deb6a060ca6f6469a59b56c6ea1e651274c94735
rosaliejames <rej3kb@virginia.edu>
2016-09-11 11:59:17 -0400

    Initial commit

---
+++ app/src/main/java/cs4720/android_mini_app_gastly/MainActivity.java
@@ -0,0 +1,13 @@
+package cs4720.android_mini_app_gastly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
